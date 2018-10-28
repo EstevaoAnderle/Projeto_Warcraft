127af9efcc3b9709629f6f2ea64874998942979d
EstevaoAnderle <estevao.anderle@gmail.com>
2018-10-28 19:28:50 -0300

    Criação do projeto.

---
+++ src/projetowarcraft/ProjetoWarcraft.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetowarcraft;
+
+/**
+ *
+ * @author estevao.050597
+ */
+public class ProjetoWarcraft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Instanciar a raça
+        
+    }
+    
+}
