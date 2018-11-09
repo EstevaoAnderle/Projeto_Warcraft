c74d011fa50f068d69e461d6f92f14a09c622909
rafael@bentonet.com.br <rafael@bentonet.com.br>
2018-11-09 08:45:32 -0200

    Correção de método getPosicao

--- src/projetowarcraft/mapa/Mapa.java
+++ src/projetowarcraft/mapa/Mapa.java
@@ -13,28 +13,30 @@ public class Mapa {
     /**
      * Acrescenta uma posição no mapa
      * @param posicao 
+     * @return a própria posição adicionada
      */
-    public void addPosicao(Posicao posicao) {
+    private Posicao addPosicao(double x, double y) {
+        Posicao posicao = new Posicao(x, y);
         this.posicoes.add(posicao);
+        return posicao;
     }
 
     /**
-     * Retorna um ArrayList de elemento(s) presente(s)
-     * na posição indicada.
+     * Retorna o objeto de uma posição desejada. Se ele não existir,
+     * cria um novo.
      * 
      * @param x
      * @param y
-     * @return Elementos da posição indicada.
+     * @return posição com X,Y desejados
      */
-    public ArrayList getPosicao(double x, double y) {
-        ArrayList elementos = new ArrayList<>();
+    public Posicao getPosicao(double x, double y) {
         for(Object p : this.posicoes){
             Posicao pos = (Posicao) p;
             if(pos.getX() == x && pos.getY() == y){
-                elementos.add(pos);
+                return pos;
             }
         }
-        return elementos;
+        return this.addPosicao(x, y);
     }
 
 }
