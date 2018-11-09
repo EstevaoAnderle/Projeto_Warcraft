7e029a4bfbf4b14bb3f9524efdbd913ad549a62c
rafael@bentonet.com.br <rafael@bentonet.com.br>
2018-11-09 08:26:32 -0200

    Início de mapa e posição

--- src/projetowarcraft/mapa/Mapa.java
+++ src/projetowarcraft/mapa/Mapa.java
@@ -2,16 +2,39 @@ package projetowarcraft.mapa;
 
 import java.util.ArrayList;
 
+/**
+ * Guarda todas as posições dos objetos presentes no jogo
+ * @author Estevão, Rafael
+ */
 public class Mapa {
 
     private ArrayList posicoes = new ArrayList<>();
 
+    /**
+     * Acrescenta uma posição no mapa
+     * @param posicao 
+     */
     public void addPosicao(Posicao posicao) {
-
+        this.posicoes.add(posicao);
     }
 
-    public void getPosicao(Posicao posicao) {
-
+    /**
+     * Retorna um ArrayList de elemento(s) presente(s)
+     * na posição indicada.
+     * 
+     * @param x
+     * @param y
+     * @return Elementos da posição indicada.
+     */
+    public ArrayList getPosicao(double x, double y) {
+        ArrayList elementos = new ArrayList<>();
+        for(Object p : this.posicoes){
+            Posicao pos = (Posicao) p;
+            if(pos.getX() == x && pos.getY() == y){
+                elementos.add(pos);
+            }
+        }
+        return elementos;
     }
 
 }
--- src/projetowarcraft/mapa/Posicao.java
+++ src/projetowarcraft/mapa/Posicao.java
@@ -1,13 +1,50 @@
 package projetowarcraft.mapa;
 
-import java.util.ArrayList;
+import java.awt.geom.Point2D;
 
-public class Posicao {
+/**
+ * Guarda a informação da posição (X,Y) de um objeto
+ * @author Estevão, Rafael
+ */
+public class Posicao extends Point2D {
 
-    private ArrayList elementos = new ArrayList<>();
+    protected double x;
+    protected double y;
+    
+    public Posicao(double x, double y){
+        this.setLocation(x, y);
+    }
+    
+    /**
+     * Define a localização X do objeto
+     * @param x
+     */
+    public void setX(double x){
+        this.x = x;
+    }
+    
+    /**
+     * Define a localização Y do objeto
+     * @param y 
+     */
+    public void setY(double y){
+        this.y = y;
+    }
+    
+    @Override
+    public double getX() {
+        return this.x;
+    }
 
-    public void getElemento() {
+    @Override
+    public double getY() {
+        return this.y;
+    }
 
+    @Override
+    public void setLocation(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
 }
